4d7dc5ef7d7f09de2dddc0f59e0f20aa6f1c00fb
DESKTOP-M3ETIL4\admin <nervergone111@gmail.com>
2024-03-05 16:53:08 +0700

    Metadata

---
+++ HYTinatt/HYTinatt/models/MetaData/OrderDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace HYTinatt.Models
+{
+    [MetadataType(typeof(OrderDetails.MetaData))]
+    public partial class OrderDetails
+    {
+        sealed class MetaData
+        {
+            [Required(AllowEmptyStrings = false, ErrorMessage = "Can not be empty OrderDetailID")]
+            public int OrderDetailID { get; set; }
+        }
+    }
+}
---
+++ HYTinatt/HYTinatt/models/MetaData/Orders.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace HYTinatt.Models.MetaData
+{
+    [MetadataType(typeof(Orders.MetaData))]
+    public partial class Orders
+    {
+        sealed class MetaData
+        {
+            [Required(AllowEmptyStrings = false, ErrorMessage = "Can not be empty OrderID")]
+            public int OrderID { get; set; }
+            [Required(AllowEmptyStrings = false, ErrorMessage = "Can not be empty TotalAmount")]
+            [Range(1,double.MaxValue, ErrorMessage = "TotalAmount is must greater than 0")]
+            public decimal TotalAmount { get; set; }
+        }
+    }
+}
